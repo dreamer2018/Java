270107045e8b5f1cf8f8c0eae229a778f3b67582
dreamer2018 <zhoupans_mail@163.com>
2015-09-11 18:51:49 +0800

    add

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: HelloWorld.java
+	> Author: ZhouPan / github:dreamer2018 
+	> Mail: zhoupans_mail@163.com
+	> Blog: blog.csdn.net/it_dream_er
+	> Created Time: Sun 06 Sep 2015 04:32:30 PM CST
+ ************************************************************************/
+import javax.swing.*;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello Java");
+    }
+}
+
