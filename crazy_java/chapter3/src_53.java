4bf4942cc3134d3154440eb6c80e7f68a72ff22d
dreamer2018 <zhoupans_mail@163.com>
2015-09-22 22:17:48 +0800

    add

---
+++ crazy_java/chapter3/src_53.java
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: src_53.java
+	> Author: ZhouPan / github:dreamer2018 
+	> Mail: zhoupans_mail@163.com
+	> Blog: blog.csdn.net/it_dream_er
+	> Created Time: Mon 21 Sep 2015 03:41:39 PM CST
+ ************************************************************************/
+
+public class src_53{
+    public static void main(String[] args){
+        int a=100;
+        float b=a;
+        System.out.println(b);
+        System.out.println("b="+b);
+        String str=5+"";
+        System.out.println(str);
+        System.out.println(3+4+"Hello");
+        System.out.println("Hello"+3+4);
+        int c=1000;
+        byte d=(byte)c;
+        System.out.println(d);
+        float e=5.23f;
+        int f=(int)e;
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
+
