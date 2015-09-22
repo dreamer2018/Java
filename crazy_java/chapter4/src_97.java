a97772a7baaa7219b8825580cb63095543914ff7
dreamer2018 <zhoupans_mail@163.com>
2015-09-22 22:18:48 +0800

    add

---
+++ crazy_java/chapter4/src_97.java
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: src_97.java
+	> Author: ZhouPan / github:dreamer2018 
+	> Mail: zhoupans_mail@163.com
+	> Blog: blog.csdn.net/it_dream_er
+	> Created Time: Mon 21 Sep 2015 11:23:22 PM CST
+ ************************************************************************/
+
+public class src_97{
+    public static void main(String[] args){
+        int[] a=new int[5];
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println("a["+i+"]="+a[i]);
+        }
+        for(int b:a)
+        {
+            System.out.println(b);
+        }
+    }
+}
+
