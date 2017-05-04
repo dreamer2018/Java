6190b07b26da12a5e22577bbe841ab65b4131e54
dreamer2018 <zhoupans_mail@163.com>
2017-05-04 15:59:07 +0800

    package test

---
+++ crazy_java/chapter5/Packagetest.java
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: Packagetest.java
+	> Author: ZhouPan
+	> GitHub: dreamer2018
+	> Blog: http://blog.csdn.net/it_dream_er
+	> Mail: zhoupan@xiyoulinux.org
+	> About: 测试Java包机制
+	> Created Time: 2017年05月04日 星期四 11时17分07秒
+ ************************************************************************/
+package test;
+
+public class Packagetest{
+    public static void main(String[] args){
+        System.out.println("Hello world\n");
+    }
+}
+
